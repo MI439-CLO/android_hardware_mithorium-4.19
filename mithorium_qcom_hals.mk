f888cf9dfc8abf0f3630e3e47f863405f19ddb6b
Jprimero15 <jprimero15@aospa.co>
2024-11-18 09:59:39 +0800

    hw: More changes
    * clone unguarded dependency here
    * remove soong namespaces because we will be using unguarded ones

--- mithorium_qcom_hals.mk
+++ mithorium_qcom_hals.mk
@@ -7,8 +7,3 @@ USE_DEVICE_SPECIFIC_MEDIA := true
 DEVICE_SPECIFIC_AUDIO_PATH := $(LOCAL_PATH)/audio-hal
 DEVICE_SPECIFIC_DISPLAY_PATH := $(LOCAL_PATH)/display-hal
 DEVICE_SPECIFIC_MEDIA_PATH := $(LOCAL_PATH)/media-hal
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys-intf/display \
-    vendor/qcom/opensource/commonsys/display \
-    vendor/qcom/opensource/display
